ae4ec010923016e57724338d414020f23eb100ed
ClarabelleCheng-Yue <cachengyue@gmail.com>
2017-03-16 13:04:04 -0700

    Add assets

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app.js';
 import './styles/style.css';
+import './assets/garden_circle_icon.png';
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
